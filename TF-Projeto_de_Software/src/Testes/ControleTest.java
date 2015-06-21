32d9e5e2d6cb3765cdc2f6bc2d07cda79a70d697
weryquessantos <weryquessantos@192.168.100.2>
2015-06-21 16:58:46 -0300

    2.3 (Alguns Testes com JUnit 4)

---
+++ TF-Projeto_de_Software/src/Testes/ControleTest.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package Testes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author weryquessantos
+ *
+ */
+public class ControleTest {
+	principal.Controle cont;
+	/**
+	 * Test method for {@link principal.Controle#opMenuValidar(int)}.
+	 */
+	@Test
+	public void testOpMenuValidar() {
+		int escolha = 1;
+		boolean retorno;
+		
+		if(escolha == 1 || escolha == 2 || escolha == 3 || escolha == 0){
+			retorno = true;
+		}
+		else{
+			retorno = false;
+		}
+		
+		assertTrue("Opção válida", retorno);
+		assertFalse("Opção inválida", !retorno);
+	}
+
+	/**
+	 * Test method for {@link principal.Controle#definirEstrategia(int, principal.PalavraContext)}.
+	 */
+	@Test
+	public void testDefinirEstrategia() {
+		int modo = 1, modo0 = 0;
+		
+		assertEquals("Estratégia Todas Letras iniciada", 1, modo);
+		assertEquals("Estratégia Letra por Letra iniciada", 0, modo0);
+	}
+
+}
